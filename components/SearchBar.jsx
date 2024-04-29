d12f0e8ded3f68376680819e3d71edf15bfc8633
Lex-KZ <azaskalkina@hotmail.com>
2024-04-29 18:26:49 +1000

    add tailwind native

---
+++ components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, TextInput } from "react-native";
+
+const SearchBar = () => {
+ return (
+    <SafeAreaView>
+        <TextInput placeholder="Search"/>
+    </SafeAreaView>
+ )
+}
+
+export default SearchBar
